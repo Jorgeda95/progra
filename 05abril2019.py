1a19eaff701231bb8c2a0ae4af425354075178af
Jorgeda95 <jorgepineda@ufm.edu>
2019-04-05 09:54:16 -0600

    git push origin master

---
+++ 05abril2019.py
@@ -0,0 +1,14 @@
+#!/...she bang
+#chmod +x sorty.py...............para dar permisos de ejecución esto nos ayuda a coorrer el archivo de forma rapida 
+#ls-l...........nos permite ver la lista y que archivos tienen permisos de ejecución
+#!/usr/bin/env python3....................para que pueda acceder al bin en cualquier lugar
+lista1=[2,100,1,-1,50]
+def sort(lista, sorting="asc"):
+    print("Sorting {}".format(lista))
+    menor=lista[0]
+    lista_ordenada=[]
+    for i in range (len(lista)):
+        print(lista[i])
+        for sliced in lista[i:]
+    
+sort(lista)
--- totito.py
+++ totito.py
@@ -1,4 +1,60 @@
-#Este es el proyecto de totito
-hi
-sdf
-#este es 
+#totito
+import random
+import tkinter
+#tkinter libreria  para interfaz gráfica
+from tkinter import *
+from tkinter import messagebox
+from tkinter import simpledialog 
+
+ventana=Tk()
+ventana.geometry("600x620")
+ventana.title("Juego del totito")
+turno = 0
+Jugador1=""
+Jugador2=""
+listaBotones = []
+tablero = [] #variables que se encontraran en el tablero
+turnoJugador = StringVar()
+for i in range(0,9):
+    tablero.append("X")
+#boton0
+boton0=Button(ventana,width=18,height=8)#command=cambiar(0)
+listaBotones.append(boton0) 
+boton0.place(x=30,y=50)
+#boton1
+boton1=Button(ventana,width=18,height=8)
+listaBotones.append(boton1)
+boton1.place(x=230,y=50)
+#boton2
+boton1=Button(ventana,width=18,height=8)
+listaBotones.append(boton1)
+boton1.place(x=430,y=50)
+#boton3
+boton0=Button(ventana,width=18,height=8)#command=cambiar(0)
+listaBotones.append(boton0) 
+boton0.place(x=30,y=250)
+#boton4
+boton1=Button(ventana,width=18,height=8)
+listaBotones.append(boton1)
+boton1.place(x=230,y=250)
+#boton5
+boton1=Button(ventana,width=18,height=8)
+listaBotones.append(boton1)
+boton1.place(x=430,y=250)
+#boton6
+boton0=Button(ventana,width=18,height=8)#command=cambiar(0)
+listaBotones.append(boton0) 
+boton0.place(x=30,y=450)
+#boton7
+boton1=Button(ventana,width=18,height=8)
+listaBotones.append(boton1)
+boton1.place(x=230,y=450)
+#boton8
+boton1=Button(ventana,width=18,height=8)
+listaBotones.append(boton1)
+boton1.place(x=430,y=450)
+
+
+
+
+ventana.mainloop()
