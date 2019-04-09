b642b2756eae46394b282c6e6454dbeb2033d3a8
Jorgeda95 <jorgepineda@ufm.edu>
2019-04-09 13:40:02 -0600

    Update totito.py
    menu diferente agregado

--- totito.py
+++ totito.py
@@ -8,12 +8,29 @@ from tkinter import simpledialog
 
 
 #acciones menu
+
     
 #ventana del menú
 vmenu=Tk()
 vmenu.title("Menú totito")
 vmenu.geometry("420x270")
 vmenu.configure(backg="#2320FC")
+
+menu=Menu(vmenu)
+vmenu.config(menu=menu) 
+
+def salir():
+    exit()
+
+subm=Menu(menu)
+menu.add_cascade(label="Menú Juego Totito", menu=subm)
+subm.add_command(label="Juego 1")
+subm.add_command(label="Juego 2")
+subm.add_command(label="Juego 3")
+subm.add_command(label="Salir del Juego", command=salir)
+
+
+
 #tablero del juego
 vtablero=Tk()
 vtablero.title("------Juego TOTITO----")
@@ -50,6 +67,8 @@ btnm0.place(x=130,y=170)
 
 
 
+# def tablero():
+
 
 #btn juego totito
 for i in range(0,9):
@@ -63,34 +82,35 @@ boton1=Button(vtablero,width=18,height=8)#command=cambiar(0)
 listaBotones.append(boton1)
 boton1.place(x=230,y=50)
 #boton2
-boton1=Button(vtablero,width=18,height=8)#command=cambiar(0)
-listaBotones.append(boton1)
-boton1.place(x=430,y=50)
+boton2=Button(vtablero,width=18,height=8)#command=cambiar(0)
+listaBotones.append(boton2)
+boton2.place(x=430,y=50)
 #boton3
-boton0=Button(vtablero,width=18,height=8)#command=cambiar(0)
-listaBotones.append(boton0) 
-boton0.place(x=30,y=250)
+boton3=Button(vtablero,width=18,height=8)#command=cambiar(0)
+listaBotones.append(boton3) 
+boton3.place(x=30,y=250)
 #boton4
-boton1=Button(vtablero,width=18,height=8)#command=cambiar(0)
-listaBotones.append(boton1)
-boton1.place(x=230,y=250)
+boton4=Button(vtablero,width=18,height=8)#command=cambiar(0)
+listaBotones.append(boton4)
+boton4.place(x=230,y=250)
 #boton5
-boton1=Button(vtablero,width=18,height=8)#command=cambiar(0)
-listaBotones.append(boton1)
-boton1.place(x=430,y=250)
+boton5=Button(vtablero,width=18,height=8)#command=cambiar(0)
+listaBotones.append(boton5)
+boton5.place(x=430,y=250)
 #boton6
-boton0=Button(vtablero,width=18,height=8)#command=cambiar(0)
-listaBotones.append(boton0) 
-boton0.place(x=30,y=450)
+boton6=Button(vtablero,width=18,height=8)#command=cambiar(0)
+listaBotones.append(boton6) 
+boton6.place(x=30,y=450)
 #boton7
-boton1=Button(vtablero,width=18,height=8)#command=cambiar(0)
-listaBotones.append(boton1)
-boton1.place(x=230,y=450)
+boton7=Button(vtablero,width=18,height=8)#command=cambiar(0)
+listaBotones.append(boton7)
+boton7.place(x=230,y=450)
 #boton8
-boton1=Button(vtablero,width=18,height=8)#command=cambiar(0)
-listaBotones.append(boton1)
-boton1.place(x=430,y=450)
+boton8=Button(vtablero,width=18,height=8)#command=cambiar(0)
+listaBotones.append(boton8)
+boton8.place(x=430,y=450)
 
 
 
 vtablero.mainloop()
+vmenu.mainloop()
